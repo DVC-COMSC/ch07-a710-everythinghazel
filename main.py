d26c02b1e5dc3ed018eed4ebf455b1ad035fd1ae
Hazel Lee <hyewon1157@gmail.com>
2022-12-13 19:21:17 -0800

    7-10

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 
-data1 = [100, 90, 80, 60]
-data2 = [90, 80, 70, 100]
+data = [[100, 90, 80, 60], [90, 80, 70, 100]]
 labels = ['Math', 'English', 'Physics', 'Computer']
 names = ['Bill', 'Mary']
 
 fig, ax = plt.subplots()
 
 # ******************************
-# Make your code
+ax.bar(labels, data[0], tag = names[0])
+ax.bar(labels,data[1],bottom=data[0],tag= names[1])
+
+ax.legend()
+ax.set_title("graph for scores")
+
 # ******************************
 
 
